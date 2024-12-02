bf08db703c80f61e0982e39f5d865810b410e110
Ana Laura Marques <lauramarques181@gmail.com>
2024-12-02 12:44:24 -0300

    add integration test and usuario repository test

--- filmes-API/src/test/java/com/adatech/filmes_API/integration/useCases/repository/UsuarioUseCaseIntegrationTest.java
+++ filmes-API/src/test/java/com/adatech/filmes_API/integration/useCases/repository/UsuarioUseCaseIntegrationTest.java
@@ -1,5 +1,8 @@
 package com.adatech.filmes_API.integration.useCases.repository;
 
+import com.adatech.filmes_API.dto.request.CriarUsuarioDTO;
+import com.adatech.filmes_API.exception.CpfDuplicadoException;
+import com.adatech.filmes_API.exception.EmailDuplicadoException;
 import com.adatech.filmes_API.exception.UsuarioNaoEncontradoException;
 import com.adatech.filmes_API.model.Usuario;
 import com.adatech.filmes_API.repository.UsuarioRepository;
@@ -25,7 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class UsuarioUseCaseIntegrationTest {
+public class  UsuarioUseCaseIntegrationTest {
 
     @MockBean
     private UsuarioRepository usuarioRepository;
@@ -47,7 +50,7 @@ public class UsuarioUseCaseIntegrationTest {
     }
 
     @Test
-    public void ClientePorEmail_realizoBusca_deveRetornarCliente() throws Exception {
+    public void buscarUsuarioPorEmail_realizoBusca_deveRetornarUsuario() throws Exception {
         Mockito.when(usuarioRepository.findByEmail("teste@gmail.com")).thenReturn(Optional.of(usuario));
 
         mockMvc.perform(
@@ -59,12 +62,12 @@ public class UsuarioUseCaseIntegrationTest {
     }
 
     @Test
-    public void ClientePorCPF_realizoBusca_deveRetornarCliente() throws Exception {
+    public void usuarioPorCPF_realizoBusca_deveRetornarUsuario() throws Exception {
         Mockito.when(usuarioRepository.findByEmail("teste@gmail.com")).thenReturn(Optional.of(usuario));
-        Mockito.when(usuarioRepository.findByCpf("273.418.830-90")).thenReturn(Optional.of(usuario));
+        Mockito.when(usuarioRepository.findByCpf("686.629.790-76")).thenReturn(Optional.of(usuario));
 
         mockMvc.perform(
-                        MockMvcRequestBuilders.get("/usuarios/cpf?cpf=273.418.830-90")
+                        MockMvcRequestBuilders.get("/usuarios/cpf?cpf=686.629.790-76")
                                 .accept(MediaType.APPLICATION_JSON)
                                 .header(HttpHeaders.AUTHORIZATION, "Basic dGVzdGVAZ21haWwuY29tOnRlc3RlMTIz")
                 )
@@ -73,14 +76,14 @@ public class UsuarioUseCaseIntegrationTest {
     }
 
     @Test
-    public void clienteNaoExiste_realizadoOCadastro_devoObterSucesso() throws Exception {
+    public void usuarioNaoExiste_realizadoOCadastro_devoObterSucesso() throws Exception {
         var usuarioJson = """
                     {
-                          "nome": "Laura",
-                          "idade": 20,
-                          "email": "laura123@gmail.com",
-                          "password": "laurinha",
-                          "cpf": "27341883090"
+                          "nome": "teste",
+                          "idade": 22,
+                          "email": "teste@gmail.com",
+                          "password": "teste123",
+                          "cpf": "686.629.790-76"
                       }
                 """;
 
@@ -94,14 +97,14 @@ public class UsuarioUseCaseIntegrationTest {
     }
 
     @Test
-    public void clienteNaoExiste_realizadoOCadastroSemNome_devoObterFalha() throws Exception {
+    public void usuarioNaoExiste_realizadoOCadastroSemNome_devoObterFalha() throws Exception {
         var usuarioJson = """
                     {
                           "nome": " ",
-                          "idade": 20,
-                          "email": "laura123@gmail.com",
-                          "password": "laurinha",
-                          "cpf": "27341883090"
+                          "idade": 22,
+                          "email": "teste@gmail.com",
+                          "password": "teste123",
+                          "cpf": "686.629.790-76"
                       }
                 """;
 
@@ -116,14 +119,14 @@ public class UsuarioUseCaseIntegrationTest {
     }
 
     @Test
-    public void clienteNaoExiste_realizadoOCadastroComNomeMenorQueTres_devoObterFalha() throws Exception {
+    public void usuarioNaoExiste_realizadoOCadastroComNomeMenorQueTres_devoObterFalha() throws Exception {
         var usuarioJson = """
                     {
-                          "nome": "la",
-                          "idade": 20,
-                          "email": "laura123@gmail.com",
-                          "password": "laurinha",
-                          "cpf": "27341883090"
+                          "nome": "te",
+                          "idade": 22,
+                          "email": "teste@gmail.com",
+                          "password": "teste123",
+                          "cpf": "686.629.790-76"
                       }
                 """;
 
@@ -138,14 +141,14 @@ public class UsuarioUseCaseIntegrationTest {
     }
 
     @Test
-    public void clienteNaoExiste_realizadoOCadastroSemEmail_devoObterFalha() throws Exception {
+    public void usuarioNaoExiste_realizadoOCadastroSemEmail_devoObterFalha() throws Exception {
         var usuarioJson = """
                     {
-                          "nome": "laura",
-                          "idade": 20,
+                          "nome": "teste",
+                          "idade": 22,
                           "email": "",
-                          "password": "laurinha",
-                          "cpf": "27341883090"
+                          "password": "teste123",
+                          "cpf": "686.629.790-76"
                       }
                 """;
 
@@ -160,14 +163,14 @@ public class UsuarioUseCaseIntegrationTest {
     }
 
     @Test
-    public void cadastroCliente_comEmailInvalido_deveRetornarErro() throws Exception {
+    public void cadastroUsuario_comEmailInvalido_deveRetornarErro() throws Exception {
         var usuarioJson = """
                     {
-                          "nome": "laura",
-                          "idade": 20,
-                          "email": "laura.gmailcom",
-                          "password": "laurinha",
-                          "cpf": "27341883090"
+                          "nome": "teste",
+                          "idade": 22,
+                          "email": "test.gmailcom",
+                          "password": "teste123",
+                          "cpf": "686.629.790-76"
                       }
                 """;
 
@@ -182,7 +185,7 @@ public class UsuarioUseCaseIntegrationTest {
     }
 
     @Test
-    public void ClientePorEmail_naoEncontrado_deveRetornarErro() throws Exception {
+    public void UsuarioPorEmail_naoEncontrado_deveRetornarErro() throws Exception {
         Mockito.when(usuarioRepository.findByEmail("teste@gmail.com"))
                 .thenReturn(Optional.of(usuario))
                 .thenThrow(new UsuarioNaoEncontradoException("Não foi possível encontrar usuário com o email"));
@@ -197,7 +200,7 @@ public class UsuarioUseCaseIntegrationTest {
     }
 
     @Test
-    public void ClientePorNome_realizoBusca_deveRetornarClientes() throws Exception {
+    public void UsuarioPorNome_realizoBusca_deveRetornarUsuarios() throws Exception {
         usuario.setNome("teste");
         Mockito.when(usuarioRepository.findByEmail("teste@gmail.com")).thenReturn(Optional.of(usuario));
         Mockito.when(usuarioRepository.findByNomeContaining("teste")).thenReturn(List.of(usuario));
@@ -210,5 +213,94 @@ public class UsuarioUseCaseIntegrationTest {
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].nome").value("teste"));
+    }
+
+    @Test
+    public void cadastroUsuario_quandoCpfInvalido_deveRetornarErro() throws Exception {
+        var usuarioJson = """
+                {
+                      "nome": "teste",
+                      "idade": 22,
+                      "email": "teste@gmail.com",
+                      "password": "teste123",
+                      "cpf": "123"
+                  }
+            """;
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/usuarios")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(usuarioJson)
+                ).andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.mensagem").value("Erro de validação nos campos"));
+    }
+
+    @Test
+    public void deletarUsuario_quandoNaoEncontrado_deveRetornarErro() throws Exception {
+        Mockito.when(usuarioRepository.findByEmail("teste@gmail.com")).thenReturn(Optional.of(usuario));
+
+        Mockito.doThrow(new UsuarioNaoEncontradoException("Usuário não encontrado."))
+                .when(usuarioRepository).deleteById(999L);
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.delete("/usuarios/999")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .header(HttpHeaders.AUTHORIZATION, "Basic dGVzdGVAZ21haWwuY29tOnRlc3RlMTIz")
+                ).andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void cadastroCliente_comEmailDuplicado_deveRetornarErro() throws Exception {
+        var usuarioJson = """
+                    {
+                          "nome": "Teste",
+                          "idade": 22,
+                          "email": "teste@gmail.com",
+                          "password": "teste123",
+                          "cpf": "686.629.790-76"
+                      }
+                """;
+
+        Mockito.when(criarUsuarioService.execute(Mockito.any(CriarUsuarioDTO.class)))
+                .thenThrow(new EmailDuplicadoException("Email já cadastrado no sistema."));
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/usuarios")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(usuarioJson)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isConflict())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.mensagem").value("Email já cadastrado no sistema."));
+    }
+
+    @Test
+    public void cadastroCliente_comCpfDuplicado_deveRetornarErro() throws Exception {
+        var usuarioJson = """
+                    {
+                          "nome": "Teste",
+                          "idade": 22,
+                          "email": "teste@gmail.com",
+                          "password": "teste123",
+                          "cpf": "686.629.790-76"
+                      }
+                """;
+
+        Mockito.when(criarUsuarioService.execute(Mockito.any(CriarUsuarioDTO.class)))
+                .thenThrow(new CpfDuplicadoException("Cpf já cadastrado no sistema."));
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/usuarios")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(usuarioJson)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isConflict())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.mensagem").value("Cpf já cadastrado no sistema."));
     }
 }
