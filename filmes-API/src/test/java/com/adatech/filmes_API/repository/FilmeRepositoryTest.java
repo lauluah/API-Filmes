b0ee4f726216df35cd06dd4728d02ca8b804d56f
Tainá Peixoto <tsouzapeixoto@gmail.com>
2024-12-05 21:19:06 -0300

    test: Implementação de testes FilmeRepositoryTest

--- filmes-API/src/test/java/com/adatech/filmes_API/repository/FilmeRepositoryTest.java
+++ filmes-API/src/test/java/com/adatech/filmes_API/repository/FilmeRepositoryTest.java
@@ -1,13 +1,63 @@
 package com.adatech.filmes_API.repository;
 
+import com.adatech.filmes_API.model.Filme;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 class FilmeRepositoryTest {
+
+    @Autowired
+    private FilmeRepository filmeRepository;
+
+    private Filme filme;
+
+    @BeforeEach
+    void setUp() {
+        filme = new Filme();
+        filme.setNomeFilme("John Wick");
+        filme.setGenero("Ação");
+        filme.setNota(8.5);
+        filme.setComentario("Excelente filme");
+        filme.setCorAvaliacao("Verde");
+        filmeRepository.save(filme);
+    }
 
     @Test
     void findByNomeFilmeContaining() {
+        List<Filme> filmes = filmeRepository.findByNomeFilmeContaining("John");
+        assertEquals(1, filmes.size());
+        assertEquals("John Wick", filmes.get(0).getNomeFilme());
     }
 
+    @Test
+    void findByGeneroContaining() {
+        List<Filme> filmes = filmeRepository.findByGeneroContaining("Ação");
+        assertEquals(1, filmes.size());
+        assertEquals("Ação", filmes.getFirst().getGenero());
+    }
+
+    @Test
+    void findByNota() {
+        List<Filme> filmes = filmeRepository.findByNota(8.5);
+        assertEquals(1, filmes.size());
+        assertEquals(8.5, filmes.getFirst().getNota());
+    }
+
+    @Test
+    void findByCorAvaliacao() {
+        List<Filme> filmes = filmeRepository.findByCorAvaliacao("Verde");
+        assertEquals(1, filmes.size());
+        assertEquals("Verde", filmes.getFirst().getCorAvaliacao());
+    }
 }
